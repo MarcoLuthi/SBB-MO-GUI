cebf76a6de36015fe1ec6350917780138088c740
Marco Luthi <macsa1@mac.com>
2013-05-30 16:52:09 +0200

    systemchange
    Time and Current Destinations

--- SBB-MO/js/stations.js
+++ SBB-MO/js/stations.js
@@ -1,28 +1,52 @@
+    function startTime()
+    {
+        var today=new Date();
+        var h=today.getHours();
+        var m=today.getMinutes();
+        var s=today.getSeconds();
+        // add a zero in front of numbers<10
+        m=checkTime(m);
+        s=checkTime(s);
+        document.getElementById('timedisplay').innerHTML=h+":"+m+":"+s;
+        t=setTimeout(function(){startTime()},500);
+        }
+
+        function checkTime(i)
+            {
+                if (i<10)
+                    {
+                        i="0" + i;
+                    }
+            return i;
+           }
+
+  
+
 
     $(function () {
 
-        var station = '008503000';
+        var station = 'Zürich HB';
 
-    	function refresh() {
-        	if (station) {
-        		$.get('http://transport.opendata.ch/v1/stationboard', {id: station, limit: 15}, function(data) {
-        		    $('#ulmain li').empty();
-        			$(data.stationboard).each(function () {
-        			    var prognosis, departure, delay, line = '<li>';
-        			    departure = moment(this.stop.departure);
-        			    if (this.stop.prognosis.departure) {
-        			        prognosis = moment(this.stop.prognosis.departure);
-        			        delay = (prognosis.valueOf() - departure.valueOf()) / 60000;
-        			        line += departure.format('HH:mm') + ' <strong>+' + delay + ' min </strong>';
-        			    } else {
-        			        line += departure.format('HH:mm');
-        			    }
-        			    line += '<span>'+ this.name + ' </span><span> ' + this.to + ' </span></li>';
-        			    $('#ulmain').append(line);
-        			});
-        		}, 'json');
-        	}
-    	}
+        function refresh() {
+            if (station) {
+                $.get('http://transport.opendata.ch/v1/stationboard?station=Zurich&transportations[]=ec_ic&transportations[]=ice_tgv_rj&transportations[]=ir&transportations[]=s_sn_r', {}, function(data) {
+                    $('#ulmain li').empty();
+                    $(data.stationboard).each(function () {
+                        var prognosis, departure, delay, line = '<li>';
+                        departure = moment(this.stop.departure);
+                        if (this.stop.prognosis.departure) {
+                            prognosis = moment(this.stop.prognosis.departure);
+                            delay = (prognosis.valueOf() - departure.valueOf()) / 60000;
+                            line += departure.format('HH:mm ') + ' <strong>+' + delay + ' min </strong>';
+                        } else {
+                            line += departure.format('HH:mm ');
+                        }
+                        line += '<span>'+ this.name + ' </span><span> ' + this.to + ' </span></li>';
+                        $('#ulmain').append(line);
+                    });
+                }, 'json');
+            }
+        }
 
         /*$('#station').autocomplete({
             source: function (request, response) {
@@ -41,6 +65,6 @@
             }
         });*/
 
-    	setInterval(refresh, 30000);
-    	refresh();
+        setInterval(refresh, 300000);
+        refresh();
     });
