c066cf8212d961eca49e46143d1974ac1bc63b5f
Marco Luthi <macsa1@mac.com>
2013-05-31 15:55:52 +0200

    Added function and animation

--- SBB-MO/js/stations.js
+++ SBB-MO/js/stations.js
@@ -1,3 +1,4 @@
+ 
     function startTime()
     {
         var today=new Date();
@@ -9,6 +10,9 @@
         s=checkTime(s);
         document.getElementById('timedisplay').innerHTML=h+":"+m+":"+s;
         t=setTimeout(function(){startTime()},500);
+        
+
+
         }
 
         function checkTime(i)
@@ -29,10 +33,10 @@
 
         function refresh() {
             if (station) {
-                $.get('http://transport.opendata.ch/v1/stationboard?station=Zurich&transportations[]=ec_ic&transportations[]=ice_tgv_rj&transportations[]=ir&transportations[]=s_sn_r', {}, function(data) {
+                $.get('http://transport.opendata.ch/v1/stationboard?station=Zurich&limit=15&transportations[]=ec_ic&transportations[]=ice_tgv_rj&transportations[]=ir&transportations[]=s_sn_r', {}, function(data) {
                     $('#ulmain li').empty();
                     $(data.stationboard).each(function () {
-                        var prognosis, departure, delay, line = '<li><span class="time">';
+                        var prognosis, departure, delay, line = '<li><a href="#test"><span class="time">';
                         departure = moment(this.stop.departure);
                         if (this.stop.prognosis.departure) {
                             prognosis = moment(this.stop.prognosis.departure);
@@ -41,7 +45,7 @@
                         } else {
                             line += departure.format('HH:mm ');
                         }
-                        line += '</span><span class="zug">'+ this.name + ' </span><span class="destination"> ' + this.to + ' </span></li>';
+                        line += '</span><span class="zug">'+ this.name + ' </span><span class="destination"> ' + this.to + ' </span></a></li>';
                         $('#ulmain').append(line);
                     });
                 }, 'json');
@@ -65,6 +69,44 @@
             }
         });*/
 
-        setInterval(refresh, 300000);
         refresh();
     });
+  
+$(document).ready(function() {
+
+    $('#rightmain')
+        .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
+        .delay(4000)           // Wait for a bit so the user notices it fade in
+        .css({"opacity":1}
+        );
+
+        $("#buttonSprache").on("click", function(e){
+        $(".sprachauswahl").toggleClass("active") //toggleClass
+    });
+
+        $("#zuruck").on("click", function(e){
+        $(".sprachauswahl").toggleClass("active") //toggleClass
+
+    });
+
+        $("#to").on("click", function(e){
+        $(".tastatur").toggleClass("active") //toggleClass
+    });
+        $("#zuruck2").on("click", function(e){
+        $(".tastatur").toggleClass("active") //toggleClass
+    });
+           $("#btnsuchen").on("click", function(e){
+        $(".leftanimation").toggleClass("hide") //toggleClass
+        $(".rightanimation").toggleClass("hide") //toggleClass
+    });
+
+          $("#ulmain").each(function(){
+            $(this).click(function(e){
+                $(".leftanimation").toggleClass("hide") //toggleClass
+    $(".rightanimation").toggleClass("hide") //toggleClass
+            });
+    });
+});
+
+
+
